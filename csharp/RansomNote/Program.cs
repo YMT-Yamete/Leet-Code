f825d7e78225aa3fb7829e4c62c0a296f775d9f1
Ye Myat Tun <ymt.yemyattun@gmail.com>
2023-06-12 17:26:56 +0630

    6/12/2023

---
+++ csharp/RansomNote/Program.cs
@@ -0,0 +1,27 @@
+﻿public class Solution
+{
+    public bool CanConstruct(string ransomNote, string magazine)
+    {
+        bool found = false;
+        for (int i = 0; i < ransomNote.Length; i++)
+        {
+            for (int j = 0; j < magazine.Length && found == false; j++)
+            {
+                if (ransomNote[i] == magazine[j])
+                {
+                    found = true;
+                    magazine = magazine.Remove(j, 1);
+                }
+            }
+            if (found == false)
+            {
+                return false;
+            }
+            else
+            {
+                found = false;
+            }
+        }
+        return true;
+    }
+}
---
+++ csharp/SummaryRanges/Program.cs
@@ -0,0 +1,50 @@
+﻿public class Solution
+{
+    public IList<string> SummaryRanges(int[] nums)
+    {
+        if (!nums.Any())
+        {
+            return new List<string>();
+        }
+        List<string> output = new List<string>();
+        List<int> tempNumList = new List<int>();
+        int? previousNum = null;
+        foreach (var item in nums)
+        {
+            if (previousNum != null)
+            {
+                if (previousNum == item - 1)
+                {
+                    tempNumList.Add(item);
+                }
+                else
+                {
+                    if (tempNumList.Count == 1)
+                    {
+                        output.Add(tempNumList.FirstOrDefault().ToString());
+                    }
+                    else if (tempNumList.Count > 1)
+                    {
+                        output.Add(tempNumList.FirstOrDefault().ToString() + "->" + tempNumList.LastOrDefault().ToString());
+                    }
+                    tempNumList.Clear();
+                    tempNumList.Add(item);
+                }
+            }
+            else
+            {
+                tempNumList.Add(item);
+            }
+            previousNum = item;
+        }
+        if (tempNumList.Count == 1)
+        {
+            output.Add(tempNumList.FirstOrDefault().ToString());
+        }
+        else if (tempNumList.Count > 1)
+        {
+            output.Add(tempNumList.FirstOrDefault().ToString() + "->" + tempNumList.LastOrDefault().ToString());
+        }
+        return output;
+    }
+}
